ae78c1e5955d1cb3144446e2ffd06f55cd153ffb
kolyjjj <me.koly.li@gmail.com>
2015-09-20 13:18:24 +0800

    add static server to serve static files

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var koa = require('koa');
 var route = require('koa-route');
+var staticServe = require('koa-static');
 var app = koa();
 
-app.use(route.get('/', function *(){
+app.use(staticServe(__dirname + '/static'));
+
+app.use(route.get('/api/', function *(){
   this.body = {msg: 'hello world'};
 }));
 
