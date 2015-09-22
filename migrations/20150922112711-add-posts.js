eb342378c1f7cde0987a66ef2c7fab2f6257c454
kolyjjj <me.koly.li@gmail.com>
2015-09-22 19:45:42 +0800

    use callback inside the migrate function and use npm script to run the migration

--- migrations/20150922112711-add-posts.js
+++ migrations/20150922112711-add-posts.js
@@ -2,13 +2,9 @@ var dbm = global.dbm || require('db-migrate');
 var type = dbm.dataType;
 
 exports.up = function(db, callback) {
-  db.createCollection('posts', function(){
-
-  });
+  db.createCollection('posts', callback);
 };
 
 exports.down = function(db, callback) {
-  db.dropCollection('posts', function(){
-
-  });
+  db.dropCollection('posts', callback);
 };
--- migrations/20150922112848-insert-posts.js
+++ migrations/20150922112848-insert-posts.js
@@ -7,19 +7,16 @@ exports.up = function(db, callback) {
     author: 'koly',
     content: '白芍药，也称白花芍药，是毛莨科芍药属植物。在我国有悠久的栽培历史，驰名中外，其称百花芍药。其根可入药。多年生草本或亚灌木，生于山坡、山谷的灌木丛或草丛中。我国大部分地区均有栽植。',
     thumbnail: '/thumbnail/shaoyao.jpg'
-  }, function(){
+  }, callback);
 
-  });
-
-  // db.insert('posts', {
-  //   title: '甘草',
-  //   author: 'Baidu',
-  //   content: '甘草，别名：国老、甜草、乌拉尔甘草、甜根子。豆科、甘草属多年生草本，根与根状茎粗壮，是一种补益中草药。对人体很好地一种药，药用部位是根及根茎，药材性状根呈圆柱形，长25~100厘米，直径0.6~3.5厘米。',
-  //   thumbnail: '/thumbnail/shaoyao.jpg'
-  // }, function(){
-  //
-  // });
+  db.insert('posts', {
+    title: '甘草',
+    author: 'Baidu',
+    content: '甘草，别名：国老、甜草、乌拉尔甘草、甜根子。豆科、甘草属多年生草本，根与根状茎粗壮，是一种补益中草药。对人体很好地一种药，药用部位是根及根茎，药材性状根呈圆柱形，长25~100厘米，直径0.6~3.5厘米。',
+    thumbnail: '/thumbnail/shaoyao.jpg'
+  }, callback);
 };
 
 exports.down = function(db, callback) {
+  callback();
 };
